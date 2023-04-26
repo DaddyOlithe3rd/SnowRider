d8266ab40dcc8062588a6c903d26bb9b263eca18
BachouVic <vic.bachand@gmail.com>
2023-04-26 18:08:01 -0400

    Update PlayerAI.cs

--- SnowRider2D/Assets/Scripts/PlayerAI.cs
+++ SnowRider2D/Assets/Scripts/PlayerAI.cs
@@ -1,67 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.XR;
+using static UnityEngine.GraphicsBuffer;
 
 public class PlayerAI : MonoBehaviour
 {
     private Rigidbody2D rb;
-    //private float MoveSpeed;
-    //private float MoveHorizontal;
+    private CapsuleCollider2D capsuleCollider;
+
     [SerializeField] float obstacleRayDistance;
-    
 
-    bool closeToWall = false;
-    bool closeToRock = false;
-    bool jumpOn;
 
+    public float jumpSpeed;
+    public float angle;  //Angle entre la normal de la collision et Vector2.right
+
+    //bool closeToWall;
+    public bool closeToRock;
+    public bool isGrounded;
+    public bool isDead;
+
+    //Les 4 Raycasts
     public GameObject obstacleRayFeet;
     public GameObject obstacleRayFrontHead;
     public GameObject obstacleRayBackHead;
     public GameObject groundRayObject;
 
-    public LayerMask obstacleLayer;
-
+    private Vector3 bottomPoint;
+    public Vector3 InitialSize;
+    public Vector2 lastNormal;
+    public Vector2 normal = Vector2.up;
+    public Vector2 lastSpeed;
 
     void Start()
     {
-        //lM = LayerMask.NameToLayer("Ground");
-        jumpOn = false;
         rb = gameObject.GetComponent<Rigidbody2D>();
-        //rb.AddForce(new Vector2(0.6f, 0f), ForceMode2D.Impulse);
+        capsuleCollider = gameObject.GetComponent<CapsuleCollider2D>();
+        isGrounded = false;
+        closeToRock = false;
+        isDead = false;
+        lastNormal = Vector2.zero;
+        lastSpeed = rb.velocity;
+        //closeToWall = false;
+        InitialSize = transform.localScale;
 
+
+        //Trouver le point bottomPoint de la capsule
+        Vector3 center = transform.position;
+        float halfHeight = capsuleCollider.size.y / 2;
+        float x = center.x + halfHeight * Mathf.Sin((transform.eulerAngles.z * Mathf.PI) / 180);
+        float y = center.y - halfHeight * Mathf.Cos((transform.eulerAngles.z * Mathf.PI) / 180);
+        bottomPoint = new Vector3(x, y, 0f);
     }
 
     void Update()
     {
-
-        if (closeToRock == true && jumpOn == true)
+        if (closeToRock)
         {
-            rb.velocity = new Vector2(rb.velocity.x, 5f);
-            jumpOn = false;
-        }
-        else if (closeToWall == true && jumpOn == true)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, 8.5f);
-            jumpOn = false;
-        }
-        else
-        {
-            rb.velocity = new Vector2(3f, rb.velocity.y);
+            rb.velocity += Vector2.up * jumpSpeed;
         }
 
-
+        //Avoir une vitesse constante
+        if (rb.velocity.magnitude < 3f)
+        {
+            rb.velocity = Vector2.right * 3.2f;
+        }
     }
-    bool OnCollisionEnter2D(Collision2D other)
-    {
-        if (obstacleLayer == (obstacleLayer | (1 << other.gameObject.layer)))
-        {
-            return true;
-        }
-        else return false;
-    }
-
     private void FixedUpdate()
     {
+        //Trouver le point bottomPoint de la capsule
+        Vector3 center = transform.position;
+        float halfHeight = capsuleCollider.size.y / 2;
+        float x = center.x + halfHeight * Mathf.Sin((transform.eulerAngles.z * Mathf.PI) / 180);
+        float y = center.y - halfHeight * Mathf.Cos((transform.eulerAngles.z * Mathf.PI) / 180);
+        bottomPoint = new Vector3(x, y, 0f);
 
 
         RaycastHit2D hitFeet = Physics2D.Raycast(obstacleRayFeet.transform.position, Vector2.right, obstacleRayDistance * 0.6f);
@@ -69,48 +83,66 @@ public class PlayerAI : MonoBehaviour
         RaycastHit2D hitBack = Physics2D.Raycast(obstacleRayBackHead.transform.position, new Vector2(0.2f, 0.5f), obstacleRayDistance);
         RaycastHit2D hitGround = Physics2D.Raycast(groundRayObject.transform.position, -Vector2.up);
 
-        Debug.DrawRay(groundRayObject.transform.position, -Vector2.up * hitGround.distance, Color.red);
-        Debug.DrawRay(obstacleRayBackHead.transform.position, new Vector2(0.2f, 0.5f) * obstacleRayDistance, Color.green);
+        ContactPoint2D[] contacts = new ContactPoint2D[10];
+        int nbContacts = rb.GetContacts(contacts);
 
-        if (hitGround.collider != null)
+        if (nbContacts == 0) isGrounded = false;
+        else
         {
-            if (hitGround.distance <= 0.03f)
+            isGrounded = true;
+            normal = contacts[nbContacts - 1].normal;
+            foreach (ContactPoint2D contact in contacts)
             {
-                jumpOn = true;
+                //if (contact.collider.gameObject)
+                //{
+                //    isDead = true;
+                //    print("Dead");
+                //}
             }
-            else
+
+            //Si on est encore sur la même pente
+            if (lastNormal != normal)
             {
-                jumpOn = false;
+                angle = (Vector2.SignedAngle(Vector2.up, normal));
+                transform.RotateAround(bottomPoint, Vector3.forward, (angle - transform.eulerAngles.z));
+
+                lastNormal = normal;
             }
         }
 
-
-        if (hitFeet.collider != null && hitFront.collider != null && jumpOn == true)
+        //Si il y a un gros obstacle devant l'AI.
+        /*if (hitFeet.collider != null && hitFront.collider != null && isGrounded == true)
         {
             Debug.DrawRay(obstacleRayFeet.transform.position, Vector2.right * obstacleRayDistance * 0.6f, Color.red);
             Debug.DrawRay(obstacleRayFrontHead.transform.position, new Vector2(0.8f, 0.4f) * obstacleRayDistance, Color.red);
             closeToWall = true;
-        }
-        else if (hitFeet.collider != null && hitFront.collider == null && jumpOn == true)
+        }*/
+
+        //S'il y a une roche devant l'AI.
+        if (hitFeet.collider != null && hitFront.collider == null && isGrounded)
         {
             Debug.DrawRay(obstacleRayFeet.transform.position, Vector2.right * obstacleRayDistance * 0.6f, Color.red);
+
             closeToRock = true;
         }
+
+        //S'il y a un arbre devant l'AI, son scale devient plus petit, comme s'il "s'accroupissait".
         else if (hitFront.collider != null && hitFeet.collider == null)
         {
             Debug.DrawRay(obstacleRayFrontHead.transform.position, new Vector2(0.8f, 0.4f) * obstacleRayDistance, Color.blue);
             Debug.DrawRay(obstacleRayFeet.transform.position, Vector2.right * obstacleRayDistance * 0.6f, Color.green);
 
-            transform.localScale = new Vector3(1f, 0.5f, 1f);
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * 0.6f, 0f);
         }
 
         else
         {
-            if (hitBack.collider == null) transform.localScale = new Vector3(1f, 1f, 1f);
-            closeToWall = false;
-            closeToRock = false;
+            if (hitBack.collider == null) transform.localScale = InitialSize;
+
             Debug.DrawRay(obstacleRayFeet.transform.position, Vector2.right * obstacleRayDistance * 0.6f, Color.green);
             Debug.DrawRay(obstacleRayFrontHead.transform.position, new Vector2(0.8f, 0.4f) * obstacleRayDistance, Color.green);
+            Debug.DrawRay(groundRayObject.transform.position, -Vector2.up * hitGround.distance, Color.blue);
+            Debug.DrawRay(obstacleRayBackHead.transform.position, new Vector2(0.2f, 0.5f) * obstacleRayDistance, Color.green);
         }
 
 
