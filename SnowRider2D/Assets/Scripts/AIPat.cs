42ec023fd9a23e2ab1a2dd0c774830f4a977070b
PatrickGorham <119078434+PatrickGorham@users.noreply.github.com>
2023-05-02 13:47:10 -0400

    comments

--- SnowRider2D/Assets/Scripts/AIPat.cs
+++ SnowRider2D/Assets/Scripts/AIPat.cs
@@ -28,40 +28,44 @@ public class AIPat : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        //obstacleRayDistance = Mathf.Sqrt((rb.velocity.x)* (rb.velocity.x) + (rb.velocity.y)* (rb.velocity.y))*100;
-        //obstacleFeetRayDistance = Mathf.Sqrt((rb.velocity.x) * (rb.velocity.x) + (rb.velocity.y) * (rb.velocity.y))*100;
-        
         obstacleUncrouchRayDistance = 0.85f;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 down = transform.TransformDirection(-Vector3.up);
     }
 
     private void Update()
     {
-        obstacleRayDistance = obstacleFeetRayDistance = rb.velocity.magnitude/2;
-        //print(rb.velocity);
         SkierController controller = GetComponentInParent<SkierController>();
+        
+        //this is the distance of the raycast that will be at the feet and the one thats going to be at the head
+        obstacleRayDistance = obstacleFeetRayDistance = rb.velocity.magnitude/2;
 
+        //Raycast to see if the player is close to the ground (used for seeing when to rotate)
         RaycastHit2D hitclosetoground = Physics2D.Raycast(closetoground.transform.position, new Vector2(0, -1), transform.localScale.x * 2, layerground);
         Debug.DrawRay(closetoground.transform.position, transform.localScale.x * 2 * new Vector2(0, -1), Color.blue);
 
+        //Raycast at the level of the head (used to detect when to crouch)
         RaycastHit2D hitObstacleHead = Physics2D.Raycast(obstacleRayObject.transform.position, rb.velocity.normalized, obstacleRayDistance, layerMask);
-        RaycastHit2D hitObstacleFeet = Physics2D.Raycast(obstacleFeetRayObject.transform.position, rb.velocity.normalized, obstacleFeetRayDistance, layerMask);
-        RaycastHit2D hitObstacleUncrouch = Physics2D.Raycast(obstacleUncrouchRayObject.transform.position, new Vector2(-1, 1), obstacleUncrouchRayDistance, layerMask);
-        RaycastHit2D hitObstacleUncrouchFront = Physics2D.Raycast(obstacleUncrouchFrontRayObject.transform.position, rb.velocity.normalized, obstacleRayDistance + 1.3f, layerMask);
-
         Debug.DrawRay(obstacleRayObject.transform.position, obstacleRayDistance * rb.velocity.normalized, Color.green);
+
+        //Raycast at the level of the feet (used to detect when to jump)
+        RaycastHit2D hitObstacleFeet = Physics2D.Raycast(obstacleFeetRayObject.transform.position, rb.velocity.normalized, obstacleFeetRayDistance, layerMask);
         Debug.DrawRay(obstacleFeetRayObject.transform.position, obstacleFeetRayDistance * rb.velocity.normalized, Color.green);
+
+        //Raycast higher than the head (used to detect if an object is still above the AI when he is crouched)
+        RaycastHit2D hitObstacleUncrouch = Physics2D.Raycast(obstacleUncrouchRayObject.transform.position, new Vector2(-1, 1), obstacleUncrouchRayDistance, layerMask);
         Debug.DrawRay(obstacleUncrouchRayObject.transform.position, new Vector2(-1, 1), Color.green);
+
+        //Raycast higher than the head (used to detect if an object is still above the AI when he is crouched)
+        RaycastHit2D hitObstacleUncrouchFront = Physics2D.Raycast(obstacleUncrouchFrontRayObject.transform.position, rb.velocity.normalized, obstacleRayDistance + 1.3f, layerMask);
         Debug.DrawRay(obstacleUncrouchFrontRayObject.transform.position, (obstacleRayDistance + 1.3f) * rb.velocity.normalized, Color.green);
+
         if (hitObstacleHead.collider != null)
         {
             Debug.DrawRay(obstacleRayObject.transform.position, obstacleRayDistance * rb.velocity.normalized, Color.red);
-            //transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * 0.6f, 0f);
             controller.crouch();
         }
         else if (hitObstacleHead.collider == null && hitObstacleUncrouch.collider == null && hitObstacleUncrouchFront.collider == null)
